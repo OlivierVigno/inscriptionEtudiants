76f54795d9f80fc54277a771764d1c70401953f6
OlivierVigno <oliviervigneault10@gmail.com>
2021-03-15 10:36:07 -0400

    commit

--- app/src/main/java/com/example/inscriptionetudiants/informationsActivity.java
+++ app/src/main/java/com/example/inscriptionetudiants/informationsActivity.java
@@ -3,7 +3,11 @@ package com.example.inscriptionetudiants;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class informationsActivity extends AppCompatActivity {
@@ -20,5 +24,14 @@ public class informationsActivity extends AppCompatActivity {
 
         TextView textActionBar = (TextView) findViewById(R.id.actionBar_text);
         textActionBar.setText("Informations sur les programmes Techniques de l'informatique");
+
+        Button retourner = (Button) findViewById(R.id.buttonRetour);
+
+        retourner.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intentMain = new Intent(informationsActivity.this, MainActivity.class);
+                startActivityForResult(intentMain, 0);
+            }
+        });
     }
 }
